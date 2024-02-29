76f2cd6badb86f1d561d97c6a4b355b41fb57217
Arunjith Surendran <arunjithsurendran22@gmail.com>
2024-02-29 10:32:05 +0530

    cors

--- server.js
+++ server.js
@@ -8,37 +8,45 @@ import todoRoute from "./routes/todoRoute.js"
 dotenv.config();
 const app = express();
 
-//middleware
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+
+// CORS configuration
+const allowedOrigins = ['https://todo-app-react-blond.vercel.app/', 'http://localhost:3000']; // Add your frontend URL here
 app.use(
   cors({
-    origin: '*',
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
 
 app.options("*", cors());
 
-// databse connection
+// Database connection
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log("database connected.....");
+    console.log("Database connected.....");
   })
   .catch((error) => {
-    console.log(error, "database disconnected.....");
+    console.log(error, "Database disconnected.....");
   });
 
-//ROUTING
-
+// Routing
 app.use("/api/v2", todoRoute);
 
-//Server connection
+// Server connection
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`server connected PORT ${PORT}`);
+  console.log(`Server connected PORT ${PORT}`);
 });
-
-
